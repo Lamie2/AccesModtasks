8ccf8f77a9d4434ec4aaac59ab405aa63b8a9cf0
Lamie2 <100703059+Lamie2@users.noreply.github.com>
2022-03-29 11:39:44 +0400

    AccessModtasks

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AccessModtask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine($"Zehmet olmasa istifadeci adinizi daxil edin");
+            string username = Console.ReadLine();
+
+            Console.WriteLine($"zehmet olmasa sifrenizi daxil edin");
+            string password = Console.ReadLine();
+
+            User user1 = new User(username);
+            user1.UserName = username;
+            user1.Password = password;
+            Console.WriteLine("Username: {user1.UserName}\nPassword: {user1.Password}");
+            Console.WriteLine(user1.HasDigit("user")); 
+            Console.WriteLine(user1.HasLower("username")); 
+            Console.WriteLine(user1.HasUpper("DHGK")); 
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccessModtask
+{
+    class User
+    {
+        public User(string username)
+        {
+            this._userName = username;
+        }
+        private string _userName;
+        private string _password;
+
+        public string UserName
+        {
+
+            get
+            {
+                return this._userName;
+            }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value) && value.Length>=6 && value.Length<=25)
+                {
+                    this._userName = value;
+
+                }
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return this._password;
+            }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value) && value.Length >= 8 && value.Length <= 25 && HasDigit("password")==true && HasUpper("password")==true 
+                    && HasLower("password")==true)
+                {
+                    this._password = value;
+                }
+            }
+
+        }
+
+        public bool HasDigit(string word)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (char.IsDigit(word[i]))
+                {
+                return true;
+                }                            
+            }
+            return false;
+        }
+
+
+        public bool HasUpper(string str1)
+        {
+            for (int i = 0; i < str1.Length; i++)
+            {
+                if (char.IsUpper(str1[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        public bool HasLower(string str2)
+        {
+            for (int i = 0; i < str2.Length; i++)
+            {
+                if (char.IsLower(str2[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+}
